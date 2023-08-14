0a9670bb388eb4ee6610412ec8b11148c2ec4a3b
ElBernie <arroues.bernard@gmail.com>
2023-08-14 11:58:16 +0200

    fix: removing user creation DTO from Users module

--- src/Users/DTO/user-create.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsEmail, IsStrongPassword } from 'class-validator';
-
-export class UserCreateDTO {
-  @IsEmail()
-  email: string;
-
-  @IsStrongPassword({ minLength: 8 })
-  password: string;
-}
