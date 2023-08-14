c7d80a5c0f2d5b7766420fd5ab4aa44bdd5af3d0
ElBernie <arroues.bernard@gmail.com>
2023-08-14 15:16:49 +0200

    feat(auth): add basic support JWT Guard

--- src/Users/users.controller.ts
+++ src/Users/users.controller.ts
@@ -1,7 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import UsersService from './users.service';
+import JwtAuthGuard from 'src/Auth/JWT.guard';
 
 @Controller('users')
 export default class UsersController {
   constructor(private usersService: UsersService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Get('/me')
+  async getCurrentUser(@Req() request) {
+    const { userId } = request.user;
+    const userData = await this.usersService.getUser(userId);
+    return userData;
+  }
 }
--- src/Users/users.service.ts
+++ src/Users/users.service.ts
@@ -1,4 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
-export default class UsersService {}
+export default class UsersService {
+  constructor(private prismaService: PrismaService) {}
+
+  async getUser(userId: string) {
+    const user = await this.prismaService.user.findUnique({
+      where: {
+        id: userId,
+      },
+    });
+
+    if (!user) throw new NotFoundException();
+
+    const { email, password, ...userData } = user;
+    return userData;
+  }
+}
