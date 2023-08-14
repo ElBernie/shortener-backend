b863335d46190bdb2b98241e9748ff7235ff6428
ElBernie <arroues.bernard@gmail.com>
2023-08-14 10:53:24 +0200

    Feat(ConfigModule):Add a ConfigModule to load env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import UsersModule from './Users/users.module';
 import LinksModule from './Links/links.module';
 
 @Module({
-  imports: [UsersModule, LinksModule],
+  imports: [ConfigModule.forRoot(), UsersModule, LinksModule],
 })
 export class AppModule {}
