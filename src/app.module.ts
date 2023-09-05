dae4d9f93049297baf1bd2cdaa67a3c75a0c8ac6
ElBernie <arroues.bernard@gmail.com>
2023-09-05 11:49:06 +0200

    feat(mail service): add a mail service with support for templates

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,26 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import PrismaModule from './Prisma/prisma.module';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
 import AuthModule from './Auth/auth.module';
 import UsersModule from './Users/users.module';
 import LinksModule from './Links/links.module';
-import PrismaModule from './Prisma/prisma.module';
 import WorkspacesModule from './Workspaces/workspaces.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    MailerModule.forRoot({
+      transport: `smtp://${process.env.SMTP_USER}:${process.env.SMTP_PASSWORD}@${process.env.SMTP_HOST}:${process.env.SMTP_PORT}`,
+      template: {
+        dir: __dirname + '/templates',
+        adapter: new HandlebarsAdapter(),
+        options: {
+          strict: true,
+        },
+      },
+    }),
     PrismaModule,
     AuthModule,
     UsersModule,
