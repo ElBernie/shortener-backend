1ef9dafaf5415a46d93534e435e9525893243ae2
ElBernie <arroues.bernard@gmail.com>
2023-09-05 10:36:38 +0200

    fix: remove service from providers, because it the parent module was already imported

--- src/Workspaces/workspaces.module.ts
+++ src/Workspaces/workspaces.module.ts
@@ -1,15 +1,14 @@
 import { Module } from '@nestjs/common';
+import LinksModule from 'src/Links/links.module';
+import UsersModule from 'src/Users/users.module';
+
 import WorkspacesController from './workspaces.controller';
 import { PrismaService } from 'src/Prisma/prisma.service';
 import WorkspacesService from './services/workspaces.service';
 import WorkspacesMembersController from './workspacesMembers.controller';
 import WorkspacesRolesService from './services/workspacesRoles.service';
 import WorkspacesMembersServices from './services/workspacesMembers.service';
-import LinksModule from 'src/Links/links.module';
-import LinksService from 'src/Links/links.service';
 import WorkspacesRolesController from './workspacesRoles.controller';
-import UsersModule from 'src/Users/users.module';
-import UsersService from 'src/Users/users.service';
 
 @Module({
   imports: [LinksModule, UsersModule],
@@ -23,8 +22,6 @@ import UsersService from 'src/Users/users.service';
     WorkspacesService,
     WorkspacesRolesService,
     WorkspacesMembersServices,
-    LinksService,
-    UsersService,
   ],
   exports: [WorkspacesService],
 })
