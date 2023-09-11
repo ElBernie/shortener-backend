f0fe4368018e95838317a7bbfe840194a091fc10
ElBernie <arroues.bernard@gmail.com>
2023-09-11 14:07:39 +0200

    feat(links-stats): create a service to get links stats

--- src/Links/links.module.ts
+++ src/Links/links.module.ts
@@ -3,10 +3,16 @@ import { PrismaService } from '../Prisma/prisma.service';
 import LinksController from './link.controller';
 import LinksService from './services/links.service';
 import WorkspacesService from 'src/Workspaces/services/workspaces.service';
+import LinksStatsService from './services/links-stats.service';
 
 @Module({
   controllers: [LinksController],
-  providers: [PrismaService, LinksService, WorkspacesService],
+  providers: [
+    PrismaService,
+    LinksService,
+    WorkspacesService,
+    LinksStatsService,
+  ],
   exports: [LinksService],
 })
 export default class LinksModule {}
